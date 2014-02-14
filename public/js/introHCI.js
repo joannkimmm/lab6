d099a9fe522e0ff9ee71515a821a615e7414f09a
Joann Kim <jnk006@ucsd.edu>
2014-02-14 21:28:35 +0000

    changes to lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,7 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
 }
 
 /*
@@ -26,7 +27,31 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	// get that URL
+	var projURL = $.get("/project/"+idNumber, projectURL);
+
 	console.log("User clicked on project " + idNumber);
+	console.log("The URL is: " + projURL);
+}
+
+/*
+ * Ajax call to get url
+ */
+function projectURL(result) {
+	// store the ID of the project in a variable
+	var projID = '#project' + result['id'];
+
+	console.log(projID);
+
+	// html to insert
+	var projHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>' + '<p>' + result['summary'] + '</p>'; 
+
+	// attach that value to # to select the correct project id
+	$(projID+' .details').html( projHTML );
 }
 
 /*
@@ -35,4 +60,22 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	var randCol = $.get("/palette", changeColors);	
+}
+
+
+/*
+ * Change the colors
+ */
+
+function changeColors(result) {
+	//capture the hex values from the array
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
